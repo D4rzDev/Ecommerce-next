51ebacc9d2c872c316afd4c50eeec7ef850ae10e
Darel Honrejas <133852063+D4rzDev@users.noreply.github.com>
2024-02-21 20:16:35 +0800

    Update interface.ts

--- app/interface.ts
+++ app/interface.ts
@@ -1,4 +1,5 @@
 export interface simplifiedProduct {
+    map(arg0: (product: any) => import("react").JSX.Element): import("react").ReactNode | Iterable<import("react").ReactNode>;
     _id:string;
     imageUrl: string;
     price: number;
